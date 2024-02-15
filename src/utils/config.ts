1c43dbb71f6b83f456d5a7354234e693cfbd061e
zkz098 <zkz20081204@126.com>
2024-02-15 23:44:59 +0800

    feat: footer

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -12,8 +12,34 @@ interface SiteConfig {
   subtitle: string;
 }
 
+interface FooterConfig {
+  since: number;
+  icon: {
+    name: string;
+  }
+  count: boolean
+  powered: boolean
+  icp: {
+    enable: boolean
+    beian: string
+    recordCode: number
+    icpnumber: string
+    icon: string
+  }
+}
+
+interface BasicConfig {
+  statics: string;
+  assets: string;
+  js: string;
+  css: string;
+}
+
+
 const config = yaml.load(fs.readFileSync("src/config.yml", "utf8")) as {
   site: Partial<SiteConfig>
+  footer: Partial<FooterConfig>
+  basic: BasicConfig
 }
 
 export const BASE_URL = import.meta.env.BASE_URL.endsWith("/")
@@ -21,3 +47,5 @@ export const BASE_URL = import.meta.env.BASE_URL.endsWith("/")
   : import.meta.env.BASE_URL;
 
 export const site = config.site
+export const footer = config.footer
+export const basic = config.basic
